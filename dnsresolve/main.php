c8f3498f22e682c72804b2c2b1af153782ee60ba
Robert (PC) <rwallhead@gmail.com>
2015-11-08 11:32:40 +1100

    More resilient microservice (aka my PHP is rusty)

--- dnsresolve/main.php
+++ dnsresolve/main.php
@@ -1,11 +1,36 @@
 <?php
-    if($_SERVER['HTTP_X_APPENGINE_INBOUND_APPID'] != 'dnstwister') {
+    if($_SERVER['HTTP_X_APPENGINE_INBOUND_APPID'] !== 'dnstwister') {
         header('Location:https://dnstwister.appspot.com');
     }
     else {
-        // Grab the ip as a string (max 15 chars).
-        $ip = substr(gethostbyname($_GET['domain']), 0, 15);
-        header('Content-type:application/json;charset=utf-8');
-        echo json_encode(array('ip'=>$ip));
+
+        $ip = null;
+
+        try {
+            // Grab the domain
+            $domain = trim(substr($_GET['domain'], 0, 255));
+
+            // If we have a testable domain passed in
+            if (strlen($domain) > 0) {
+
+                // Attempt a gethostbyname()
+                $ip_str = substr(gethostbyname($_GET['domain']), 0, 15);
+
+                // Check we got an actual valid IP address
+                if (filter_var($ip_str, FILTER_VALIDATE_IP, FILTER_VALID_IPV4) !== false) {
+                    $ip = $ip_str;
+                }
+            }
+            else {
+                echo 'invalid url';
+            }
+        }
+        catch (Exception $e)
+        {
+            //no-op
+        }
+
+        echo json_encode(array('ip'=>$ip))
+
     }
 ?>
