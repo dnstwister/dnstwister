88e5abfce19d5817eabd2d419c4aa601f0bff0f9
Robert (PC) <rwallhead@gmail.com>
2015-11-08 12:55:20 +1100

    Moved templates

--- dnstwister/main.py
+++ dnstwister/main.py
@@ -11,7 +11,10 @@ import webapp2
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader=jinja2.FileSystemLoader(os.path.join((
+        os.path.dirname(__file__),
+        'templates'
+    ))),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
