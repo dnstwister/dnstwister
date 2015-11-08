368171417846dc8edd0df924fa9332717abdc5c4
Robert (PC) <rwallhead@gmail.com>
2015-11-08 13:57:14 +1100

    Better presentation of async resolution code

--- dnstwister/static/script.js
+++ dnstwister/static/script.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
+    var count = $('.resolvable').length;
+    var found = 0;
     $('.resolvable').each(function() {
         var elem = $(this)
         var domain = elem.data('domain');
         $.getJSON('/ip', {'domain': domain}, function(result) {
             if (result.ip !== null) {
                 elem.text(result.ip);
+                elem.parent().show();
+                found += 1;
             }
-            else {
-                elem.text('no');
-            }
+            count -= 1;
         });
     });
+
+    var timer = null;
+    timer = setInterval(function() {
+        if (count === 0) {
+            clearInterval(timer);
+            if (found === 0) {
+                $('.progress td').text('No IP Addresses resolved');
+            }
+            else {
+                $('.progress').hide();
+            }
+        }
+    }, 100);
 });
