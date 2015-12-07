44a593b0a36a3b3c6a03e01b78cf5e3dd2c279bb
Robert (PC) <rwallhead@gmail.com>
2015-12-07 20:35:09 +1100

    Added show-all button #1

--- dnstwister/static/script.js
+++ dnstwister/static/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+
+    // UI hooks
+    $('#show_all').click(function() {
+        $('.report').show();
+        $('.report tr').toggleClass('displayed');
+    });
+
+    // DNS resolution
     var to_resolve = $('.resolvable').length;
     var resolvable = to_resolve;
     var found = 0;
@@ -10,15 +18,18 @@ $(document).ready(function() {
         $.getJSON('/ip', {'b64': domainb64}, function(result) {
             if (result.ip !== null) {
                 elem.text(result.ip);
-                elem.parent().show();
+                elem.parent().addClass('resolved');
                 $('.report').show();
                 found += 1;
             }
             else if (result.error !== false) {
-                elem.text('?');
+                elem.text('Resolution error!');
                 elem.attr('title', 'There was an error resolving this IP');
-                elem.parent().show();
+                elem.parent().addClass('error');
                 $('.report').show();
+            }
+            else {
+                elem.text('None');
             }
             to_resolve -= 1;
             $('.resolved_count').text(resolvable - to_resolve);
