f606085d90ee5980906efaf0136d4235aec1807c
Robert (PC) <rwallhead@gmail.com>
2015-12-13 13:31:59 +1100

    Added tests for the report generator

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -3,6 +3,7 @@
 import base64
 import dnstwister.dnstwist as dnstwist
 import dnstwister.tools as tools
+import operator
 import unittest
 
 
@@ -120,4 +121,42 @@ class TestTools(unittest.TestCase):
             'www.example.com',
             tools.parse_domain({'b64': domain_data}),
             'b64-decodable valid domain in b64 key should be returned'
+        )
+
+
+    def test_analyse(self):
+        """ Test the tool that generates the reports.
+        """
+        domain = 'a.com'
+        results = tools.analyse(domain)
+
+        self.assertEqual(
+            'a.com', results[0],
+            'First item in results should be the original domain'
+        )
+
+        self.assertEqual(
+            ['fuzzy_domains'],
+            results[1].keys(),
+            'We only return fuzzy domains in report'
+        )
+
+        self.assertItemsEqual(
+            {'domain': 'a.com', 'fuzzer': 'Original*', 'b64': 'YS5jb20='},
+            results[1]['fuzzy_domains'][0],
+            'First result is the original domain'
+        )
+
+        self.assertItemsEqual(
+            [
+                'a.com', 'c.com', 'e.com', 'i.com', 'q.com', 'aa.com',
+                'w.com', 's.com', 'z.com', 'wwwa.com'
+            ],
+            map(operator.itemgetter('domain'), results[1]['fuzzy_domains']),
+            'We have 10 results including the original domain'
+        )
+
+        self.assertIs(
+            None, tools.analyse('\\.38iusd-s-da   aswd?'),
+            'Invalid domains return None'
         )
