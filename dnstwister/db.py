8f83f20cc0843971a040bbd1c53567fc852b741d
Robert Wallhead <rwallhead@gmail.com>
2016-02-08 10:04:56 +1100

    Trying to get my head around the correct application of psycopg2 pools for connections

--- dnstwister/db.py
+++ dnstwister/db.py
@@ -1,66 +1,80 @@
 """Storage of results for comparison and CUD alerting."""
+import contextlib
 import datetime
 import hashlib
 import json
 import os
-import psycopg2
+import psycopg2.pool
 import urlparse
 
 
 urlparse.uses_netloc.append('postgres')
-url = urlparse.urlparse(os.environ['DATABASE_URL'])
+DB_URL = urlparse.urlparse(os.environ['DATABASE_URL'])
 
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
+DB_CONN_POOL = psycopg2.pool.ThreadedConnectionPool(
+    1,
+    10,
+    database=DB_URL.path[1:],
+    user=DB_URL.username,
+    password=DB_URL.password,
+    host=DB_URL.hostname,
+    port=DB_URL.port,
 )
+
+
+@contextlib.contextmanager
+def cursor():
+    """Return a database cursor."""
+    try:
+        conn = DB_CONN_POOL.getconn()
+        cur = conn.cursor()
+        try:
+            yield cur
+        finally:
+            cur.close()
+    finally:
+        DB_CONN_POOL.putconn(conn)
 
 
 def stored_set(domain, result):
     """Store a result for a domain."""
-    cur = conn.cursor()
-    if stored_exists(domain):
-        cur.execute("""
-            UPDATE stored
-            SET result = (%s)
-            WHERE domain = (%s);
-        """, (json.dumps(result), domain))
-    else:
-        cur.execute("""
-            INSERT INTO stored (domain, result)
-            VALUES (%s, %s);
-        """, (domain, json.dumps(result)))
-    conn.commit()
-    cur.close()
+    with cursor() as cur:
+        if stored_exists(domain):
+            cur.execute("""
+                UPDATE stored
+                SET result = (%s)
+                WHERE domain = (%s);
+            """, (json.dumps(result), domain))
+        else:
+            cur.execute("""
+                INSERT INTO stored (domain, result)
+                VALUES (%s, %s);
+            """, (domain, json.dumps(result)))
 
 
 def stored_exists(domain):
     """Return whether a domain has results recorded."""
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT domain
-        FROM stored
-        where domain = (%s);
-    """, (domain,))
-    return cur.fetchone() is not None
+    with cursor() as cur:
+        cur.execute("""
+            SELECT domain
+            FROM stored
+            where domain = (%s);
+        """, (domain,))
+        return cur.fetchone() is not None
 
 
 def stored_get(domain):
     """Return stored results for a domain or None if none exist."""
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT result
-        FROM stored
-        WHERE domain = (%s);
-    """, (domain,))
-    (result,) = cur.fetchone()
-    if result is None:
-        return
-    return json.loads(result)
+    with cursor() as cur:
+        cur.execute("""
+            SELECT result
+            FROM stored
+            WHERE domain = (%s);
+        """, (domain,))
+        (result,) = cur.fetchone()
+        if result is None:
+            return
+        return json.loads(result)
 
 
 def subscription_new(domain, auth_len=100):
@@ -70,11 +84,10 @@ def subscription_new(domain, auth_len=100):
     auth_hash = bytearray(hashlib.sha512(auth).digest())
     expires = datetime.datetime.now() + datetime.timedelta(days=365)
 
-    cur = conn.cursor()
-    cur.execute("""
-        INSERT INTO subscriptions (auth_hash, domain, expires)
-        VALUES (%s, %s, %s)
-    """, (auth_hash, domain, expires))
-    conn.commit()
+    with cursor() as cur:
+        cur.execute("""
+            INSERT INTO subscriptions (auth_hash, domain, expires)
+            VALUES (%s, %s, %s)
+        """, (auth_hash, domain, expires))
 
     return auth
