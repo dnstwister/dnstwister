195d972ee7205095d5b583edfe7ac3f6a856abc0
Robert Wallhead <rwallhead@gmail.com>
2016-02-10 20:04:03 +1100

    Working a tweaked worker that only updates the RSS feed reports every 24 hours
    Set to 60 seconds for initial test

--- dnstwister/worker_atom.py
+++ dnstwister/worker_atom.py
@@ -1,19 +1,47 @@
 """Updates atom feeds."""
-import collections
+import datetime
 import time
 
 import db
 import tools
 
 
+# Time in seconds between re-processing a domain.
+PERIOD = 60#86400
+
+
 if __name__ == '__main__':
     while True:
-        time.sleep(1)
+
         try:
             start = time.time()
 
             # Pick the oldest domain.
-            domain = db.stored_oldest()
+            with db.cursor() as cursor:
+
+                threshold = (
+                    datetime.datetime.now() -
+                    datetime.timedelta(seconds=PERIOD)
+                )
+
+                # Get the first entry with an updated date older than the
+                # threshold.
+                cursor.execute("""
+                    SELECT domain
+                    FROM stored
+                    WHERE updated < (%s)
+                    ORDER BY updated ASC
+                    LIMIT 1
+                """, (threshold,))
+
+                result = cursor.fetchone()
+
+                # If we're idle, that's great.
+                if result is None:
+                    time.sleep(1)
+                    continue
+
+                domain = result[0]
 
             # Generate a new report.
             latest = {}
@@ -32,4 +60,5 @@ if __name__ == '__main__':
 
         except Exception as ex:
             db.DB = None
+            time.sleep(1)
             print 'crashed... {}'.format(ex)
