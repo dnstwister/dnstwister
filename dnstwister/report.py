55315ad8a9805f5671fd8a7356be9497919d92d9
Robert Wallhead <rwallhead@gmail.com>
2016-02-21 09:40:12 +1100

    Created a reporting module
    This sits between the views and the database to provide the reporting CRUD ops

---
+++ dnstwister/report.py
@@ -0,0 +1,49 @@
+"""URL report management."""
+import datetime
+
+
+import main
+db = main.storage
+
+
+REGISTER_UPDATE_DATE = datetime.datetime(1970, 1, 1)
+
+
+def oldest(min_age=86400):
+    """ Return the oldest URL that hasn't been updated for more than min_age.
+
+    Return None if all reports are up to date.
+
+    min_age is in seconds, defaults to 24 hours.
+    """
+    delta = datetime.timedelta(seconds=min_age)
+    try:
+        domain, updated = db.reports.oldest()
+        if datetime.datetime.now() - updated > delta:
+            return domain
+    except TypeError:
+        # No reports
+        pass
+
+
+def update(domain, report, updated=None):
+    """Update the report for a URL.
+
+    Args:
+        domain: the domain (as string) being reported on
+        report: the generated report (a dict)
+        updated: when the report was updated. Defaults to now.
+    """
+    if updated is None:
+        updated = datetime.datetime.now()
+    db.reports.update(domain, report, updated)
+
+
+def get(domain):
+    """Return the report for a URL, or None if one hasn't been created yet."""
+    return db.reports.get(domain)
+
+
+def register(domain):
+    """Add a domain to the reports storage, with no report data."""
+    db.reports.new(domain, REGISTER_UPDATE_DATE)
