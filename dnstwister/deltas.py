8859dd8b5f99f8184e8a45e5165ebc27d613b866
Robert Wallhead <rwallhead@gmail.com>
2016-02-21 16:07:59 +1100

    This confirms that some errors are being swallowed

--- dnstwister/deltas.py
+++ dnstwister/deltas.py
@@ -18,13 +18,12 @@ def oldest(min_age=86400):
     min_age is in seconds, defaults to 24 hours.
     """
     age_delta = datetime.timedelta(seconds=min_age)
-
     domain, generated = db.deltas.oldest()
     if datetime.datetime.now() - generated > age_delta:
         return domain
 
     print 'No domains to resolve for approx {} seconds'.format(
-        age_delta - int(datetime.datetime.now() - generated)
+        age_delta - int((datetime.datetime.now() - generated).total_seconds())
     )
 
 
