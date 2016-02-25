f276b95682091102119262e6d6f2b333fafdf0dd
Robert Wallhead <rwallhead@gmail.com>
2016-02-25 20:32:43 +1100

    First test of the atom registration
    Will check it works in Travis etc before continuing

---
+++ tests/patches.py
@@ -0,0 +1,20 @@
+"""Mocks."""
+class DeltasDB(object):
+    """Replace the main storage with a lightweight in-memory shim."""
+    def __init__(self):
+        self._db = {}
+
+    def get(self, domain):
+        try:
+            return self._db[domain][0]
+        except KeyError:
+            pass
+
+    def set(self, domain, delta, generated):
+        self._db[domain] = (delta, generated)
+
+    def reset(self):
+        self._db = {}
+
+
+deltas = DeltasDB()
--- tests/test_atom.py
+++ tests/test_atom.py
@@ -1,19 +1,61 @@
 """Tests of the atom behaviour."""
 import base64
+import datetime
 import flask.ext.webtest
+import mock
+import patches
+import textwrap
 import unittest
+import xml.dom.minidom
 
 import dnstwister.main
+
 
 class TestAtom(unittest.TestCase):
     """Tests of the atom feed behaviour."""
     def setUp(self):
         """Set up the mock memcache."""
+        # Reset the test databases
+        patches.deltas.reset()
+
         # Create a webtest Test App for use
         self.app = flask.ext.webtest.TestApp(dnstwister.main.app)
 
+    @mock.patch('dnstwister.main.storage.pg_database.deltas', patches.deltas)
     def test_new_feed(self):
         """Tests the registration of a new feed."""
-
         # We need a domain to get the feed for.
         domain = 'www.example.com'
+
+        # A feed is registered by trying to load it (and it not already being
+        # registered).
+        res = self.app.get('/atom/{}'.format(base64.b64encode(domain)))
+
+        # And only returns a single placeholder item.
+        assert str(res) == textwrap.dedent("""
+            Response: 200 OK
+            Content-Type: application/atom+xml; charset=utf-8
+            <?xml version="1.0" encoding="utf-8"?>
+            <feed xmlns="http://www.w3.org/2005/Atom">
+              <title type="text">DNS Twister report for www.example.com</title>
+              <id>https://dnstwister.report/atom/d3d3LmV4YW1wbGUuY29t</id>
+              <updated>{date_today}</updated>
+              <link href="https://dnstwister.report/report/?q=d3d3LmV4YW1wbGUuY29t" />
+              <link href="https://dnstwister.report/atom/d3d3LmV4YW1wbGUuY29t" rel="self" />
+              <generator>Werkzeug</generator>
+              <entry xml:base="https://dnstwister.report/atom/d3d3LmV4YW1wbGUuY29t">
+                <title type="text">No report yet for www.example.com</title>
+                <id>waiting:www.example.com</id>
+                <updated>{date_today}</updated>
+                <published>{date_today}</published>
+                <author>
+                  <name>DNS Twister</name>
+                </author>
+                <content type="text">Your report feed will be generated within 24 hours.</content>
+              </entry>
+            </feed>
+        """).strip().format(
+            date_today=datetime.datetime.now().replace(
+                hour=0, minute=0, second=0, microsecond=0
+            ).strftime('%Y-%m-%dT%H:%M:%SZ')
+        )
