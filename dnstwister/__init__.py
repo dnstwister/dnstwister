8d9c77fc97e08225fcce170e0ba56185794c8ea1
Robert Wallhead <rwallhead@gmail.com>
2016-07-02 12:17:58 +1000

    Trailing some logging tweaks - i think it's waitress blocking info logs

--- dnstwister/__init__.py
+++ dnstwister/__init__.py
@@ -17,9 +17,11 @@ emailer = mail.sendgridservice.SGSender()
 # Logging
 @app.before_first_request
 def setup_logging():
-    """Enable info-level logging."""
+    """Enable info-level logging in waitress."""
     if not app.debug:
-        # In production mode, add log handler to sys.stderr.
+        #logger = logging.getLogger('waitress')
+        #logger.setLevel(logging.INFO)
+        print 'LOGGERS', app.logger.handlers
         app.logger.addHandler(logging.StreamHandler())
         app.logger.setLevel(logging.INFO)
 
