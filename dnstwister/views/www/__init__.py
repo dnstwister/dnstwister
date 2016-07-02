2ad73d88ee0468641c4469245b195708b4e7dd80
Robert Wallhead <rwallhead@gmail.com>
2016-07-02 10:37:51 +1000

    Added paths from root for favicon, robots.txt and sitemap.xml

---
+++ dnstwister/views/www/__init__.py
@@ -0,0 +1,16 @@
+"""Root resources."""
+import flask
+
+from dnstwister import app
+from dnstwister import cache
+
+
+@cache.memoize(86400)
+@app.route(r'/favicon.ico')
+@app.route(r'/robots.txt')
+@app.route(r'/sitemap.xml')
+def favicon():
+    """Favicon (because some clients don't read the link tag) as well as some
+    other static files.
+    """
+    return flask.send_from_directory(app.static_folder, flask.request.path[1:])
