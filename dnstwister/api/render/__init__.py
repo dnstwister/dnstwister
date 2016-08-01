cffbd2f191c880799811fd1a917805cf55a04a7b
Robert Wallhead <rwallhead@gmail.com>
2016-08-01 11:42:00 +1000

    Creating thumbnail of rendered website

--- dnstwister/api/render/__init__.py
+++ dnstwister/api/render/__init__.py
@@ -1,11 +1,30 @@
 """Page renderer"""
 import base64
+import io
 import os
 import subprocess
+import warnings
+import PIL.Image
 
+
+warnings.simplefilter('error', PIL.Image.DecompressionBombWarning)
 
 EXE = 'phantomjs'
 MAX_DATA = 1024 * 1024  # 1MB
+THUMB = (320, 240)
+
+
+def thumbnail(image):
+    """Reduce the size of the image to a suitable thumbnail."""
+    p_image = PIL.Image.open(io.BytesIO(image))
+    assert p_image.format == 'PNG'
+
+    p_image.thumbnail(THUMB)
+
+    thumb = io.BytesIO()
+    p_image.save(thumb, format='png')
+
+    return thumb.getvalue()
 
 
 def render(domain):
@@ -17,4 +36,5 @@ def render(domain):
     ]
     payload = subprocess.check_output(run_path)[:MAX_DATA]
     image = base64.b64decode(payload)
+    image = thumbnail(image)
     return image
