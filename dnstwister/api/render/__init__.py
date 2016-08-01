508c5f8303dbe8f5c9e1142edf25533c99c40fb8
Robert Wallhead <rwallhead@gmail.com>
2016-08-01 11:09:46 +1000

    Limited screenshot size to 1MB max

--- dnstwister/api/render/__init__.py
+++ dnstwister/api/render/__init__.py
@@ -5,6 +5,7 @@ import subprocess
 
 
 EXE = 'phantomjs'
+MAX_DATA = 1024 * 1024  # 1MB
 
 
 def render(domain):
@@ -14,6 +15,6 @@ def render(domain):
         os.path.join(os.path.dirname(__file__), 'render.js'),
         'http://{}'.format(domain)
     ]
-    payload = subprocess.check_output(run_path)
+    payload = subprocess.check_output(run_path)[:MAX_DATA]
     image = base64.b64decode(payload)
     return image
