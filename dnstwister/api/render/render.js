eb5b5910941ca5ae1efe9ecf1cf0a8472ac13c40
Robert Wallhead <rwallhead@gmail.com>
2016-08-01 10:30:10 +1000

    Nicer page renderer

--- dnstwister/api/render/render.js
+++ dnstwister/api/render/render.js
@@ -1,19 +1,41 @@
-var system = require('system');
-var args = system.args;
-var url = args[1];
+// See dnstwist.py's validate_domain().
+function validateUrlArg(url) {
+  if (url.length > 255 || url.length === 0) {
+    return false;
+  }
 
-var page = require('webpage').create();
-page.viewportSize = {
-  width: 800,
-  height: 600,
-};
-page.clipRect = {
-  top: 0,
-  left: 0,
-  width: 800,
-  height: 600
-};
-page.open(url, function() {
-  console.log(page.renderBase64('png'));
+  if (url[url.length - 1] === '.') {
+    url = url.slice(0, -1);
+  }
+
+  var urlValidation = /^http:\/\/([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/;
+  return urlValidation.test(url);
+}
+
+try {
+
+  var page = require('webpage').create();
+  page.viewportSize = { width: 800, height: 600 };
+  page.clipRect = { top: 0, left: 0, width: 800, height: 600 };
+
+  var system = require('system');
+  var args = system.args;
+  if (args.length !== 2) {
+    phantom.exit();
+  }
+
+  var url = args[1];
+
+  if (validateUrlArg(url) !== true) {
+    phantom.exit();
+  }
+
+  page.open(url, function() {
+    console.log(page.renderBase64('png'));
+    phantom.exit();
+  });
+
+}
+catch (err) {
   phantom.exit();
-});
+}
