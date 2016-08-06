3bee9baf9fbf054bcf1a6775824e1d49f991557f
Robert Wallhead <rwallhead@gmail.com>
2016-08-06 13:06:50 +1000

    Suppressing remote page js errors

--- dnstwister/api/render/render.js
+++ dnstwister/api/render/render.js
@@ -13,7 +13,10 @@ function validateUrlArg(url) {
 }
 
 try {
+  phantom.onError = function() {};
+
   var page = require('webpage').create();
+  page.onError = function () {};
   page.viewportSize = { width: 800, height: 600 };
   page.clipRect = { top: 0, left: 0, width: 800, height: 600 };
 
