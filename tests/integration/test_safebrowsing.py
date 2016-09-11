da2a5b79e227ae42ab48b6110a85a2220098798e
Robert Wallhead <rwallhead@gmail.com>
2016-09-11 10:56:31 +1000

    Added test of Google Safebrowsing API

--- tests/integration/test_safebrowsing.py
+++ tests/integration/test_safebrowsing.py
@@ -1,8 +1,8 @@
-"""Parked check integration test."""
+"""Google Safebrowsing integration test."""
 
 
-def test_parked_query(webapp):
-    request = webapp.get('/api/parked/dnstwister.report')
+def test_safebrowsing_query(webapp):
+    request = webapp.get('/api/safebrowsing/dnstwister.report')
 
     assert request.status_code == 200
 
@@ -10,10 +10,8 @@ def test_parked_query(webapp):
         u'domain': u'dnstwister.report',
         u'domain_as_hexadecimal': u'646e73747769737465722e7265706f7274',
         u'fuzz_url': u'http://localhost:80/api/fuzz/646e73747769737465722e7265706f7274',
-        u'redirects': False,
-        u'redirects_to': None,
+        u'issue_detected': False,
+        u'parked_score_url': u'http://localhost:80/api/parked/646e73747769737465722e7265706f7274',
         u'resolve_ip_url': u'http://localhost:80/api/ip/646e73747769737465722e7265706f7274',
-        u'score': 0.07,
-        u'score_text': u'Possibly',
-        u'url': u'http://localhost:80/api/parked/dnstwister.report'
+        u'url': u'http://localhost:80/api/safebrowsing/dnstwister.report'
     }
