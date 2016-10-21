a95a9f4fa3754260813daff67de621f9b353dfb8
Robert Wallhead <rwallhead@gmail.com>
2016-10-21 12:31:00 +1100

    More tests of the suggestion engine

--- tests/test_search_outliers.py
+++ tests/test_search_outliers.py
@@ -42,7 +42,7 @@ def test_suggestion(webapp):
 
 
 def test_no_suggestion_long_words(webapp):
-    """Test long search terms are dropped in suggestions"""
+    """Test long search terms are dropped in suggestions."""
     query = 'jsdfijsdlfkjsoijsldjlsdkjflskdjfoewjfsdfldkishgisuehgdsfsdkf'
     response = webapp.post('/search', {'domains': query})
 
@@ -60,10 +60,28 @@ def test_no_suggestion_long_words(webapp):
 
 
 def test_no_valid_domains_only(webapp):
-    """Test invalid domains not in suggestions"""
+    """Test invalid domains not in suggestions."""
     query = 'abc ?@<>.'
     response = webapp.post('/search', {'domains': query})
 
     assert response.status_code == 302
     assert response.headers['location'].endswith('=6162632e636f6d')
     assert binascii.unhexlify('6162632e636f6d') == 'abc.com'
+
+
+def test_suggestion_rendered(webapp):
+    """Test suggestion rendered on index."""
+    response = webapp.post('/search', {'domains': 'example'})
+    next_url = response.headers['location']
+    response = webapp.get(next_url)
+
+    assert 'example.com' in response.body
+
+
+def test_get_errors(webapp):
+    """Test funny URLs for a GET search."""
+    response = webapp.get('/search/__<<>')
+
+    assert response.status_code == 302
+    assert response.headers['location'] == 'http://localhost:80/error/0'
+
