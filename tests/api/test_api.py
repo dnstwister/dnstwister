10f06c4570a833e563d485f6d1ed13532d427b6a
Robert Wallhead <rwallhead@gmail.com>
2016-11-05 11:43:51 +1100

    Added testing of the domain validation across API endpoints

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,4 +1,6 @@
 """The API's basic endpoint."""
+import pytest
+import webtest.app
 
 
 def test_api_root(webapp):
@@ -20,3 +22,13 @@ def test_api_root_redirect(webapp):
 
     assert request.status_code == 301
     assert request.headers['location'] == 'http://localhost/api/'
+
+
+def test_api_domain_validation(webapp):
+    """Test that domains are validated on all API endpoints."""
+    malformed_domain = 'example'
+    endpoints = ('fuzz', 'to_hex', 'ip', 'parked', 'safebrowsing', 'whois')
+    for endpoint in endpoints:
+        with pytest.raises(webtest.app.AppError) as err:
+            webapp.get('/api/{}/{}'.format(endpoint, malformed_domain))
+        assert '400 BAD REQUEST' in err.value.message
