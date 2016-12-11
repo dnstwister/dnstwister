cff34a6b0e1884e5e6f638bf45ab493f70304e75
Robert Wallhead <rwallhead@gmail.com>
2016-12-11 11:35:27 +1100

    First pass at a worker for updating the stats themselves

---
+++ dnstwister/workers/stats/update_deltas.py
@@ -0,0 +1,45 @@
+"""Update the deltas on all stats records."""
+import time
+
+from dnstwister import repository
+from dnstwister.repository import domain_stats
+from dnstwister.tools import noisy_domains
+
+
+def process_domain(domain):
+    """Update the window on this domain."""
+    delta_report = repository.get_delta_report(domain)
+    if delta_report is None:
+        return
+
+    for domain in delta_report.keys():
+
+        current_stats = domain_stats.get_noise_stats(domain)
+        if current_stats is None:
+            current_stats = noisy_domains.initialise_record(domain)
+
+        updated_stats = noisy_domains.increment(current_stats)
+        domain_stats.set_noise_stats(updated_stats)
+
+
+def main():
+    """Main code for worker."""
+    while True:
+
+        domains_iter = repository.iregistered_domains()
+
+        while True:
+            try:
+                domain = domains_iter.next()
+            except StopIteration:
+                break
+
+            process_domain(domain)
+
+        print 'All deltas processed'
+
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
