697e32d1d48bb58ce5ea860870e865c297b7448c
Robert Wallhead <rwallhead@gmail.com>
2016-12-27 20:51:33 +1100

    Testing some of the worker edge-cases
    #21

--- tests/test_worker_statistics.py
+++ tests/test_worker_statistics.py
@@ -1,4 +1,6 @@
 """Tests of the statistics worker."""
+import datetime
+
 import dnstwister
 import patches
 import workers.statistics
@@ -16,7 +18,7 @@ def set_up_mocks(monkeypatch):
 
 
 def test_new_email_sub_creates_new_statistics(capsys, monkeypatch):
-    """A new email sub will create a new set of stats."""
+    """A new email sub will create a new set of statistics."""
     data_repository, statistics_repository = set_up_mocks(monkeypatch)
 
     # GIVEN a subscribed user with a delta report created.
@@ -37,7 +39,7 @@ def test_new_email_sub_creates_new_statistics(capsys, monkeypatch):
 
 
 def test_statistics_worker_handles_no_delta_report(capsys, monkeypatch):
-    """A new email sub will create a new set of stats."""
+    """No delta report just means no statistics are generated."""
     data_repository, statistics_repository = set_up_mocks(monkeypatch)
 
     # GIVEN a subscribed user (and no delta report created).
@@ -48,3 +50,54 @@ def test_statistics_worker_handles_no_delta_report(capsys, monkeypatch):
 
     # THEN nothing happens.
     assert statistics_repository.get_noise_stat('exxample.com') is None
+
+
+def test_statistics_dont_update_in_under_a_day(capsys, monkeypatch):
+    """Statistics will not update more than once a day."""
+    data_repository, statistics_repository = set_up_mocks(monkeypatch)
+
+    # GIVEN a subscribed user with a delta report created.
+    data_repository.subscribe_email('1234', 'a@b.com', 'example.com')
+    data_repository.update_delta_report('example.com', {
+        'new': [['exxample.com', '127.0.0.1']],
+        'updated': [],
+        'deleted': [],
+    })
+
+    # GIVEN the statistic was checked within one day.
+    now = datetime.datetime.now() - datetime.timedelta(hours=23)
+    statistics_repository.mark_noise_stat_as_updated('exxample.com', now=now)
+
+    # WHEN the email work is ran
+    workers.statistics.increment_email_sub_deltas()
+
+    # THEN no statistics will be created for exxample.com, even though there
+    # is (now) a delta report.
+    assert statistics_repository.get_noise_stat('exxample.com') is None
+
+
+def test_statistics_dont_update_once_a_day(capsys, monkeypatch):
+    """Statistics will only update once a day."""
+    data_repository, statistics_repository = set_up_mocks(monkeypatch)
+
+    # GIVEN a subscribed user with a delta report created.
+    data_repository.subscribe_email('1234', 'a@b.com', 'example.com')
+    data_repository.update_delta_report('example.com', {
+        'new': [['exxample.com', '127.0.0.1']],
+        'updated': [],
+        'deleted': [],
+    })
+
+    # GIVEN the statistic was checked within one day.
+    now = datetime.datetime.now() - datetime.timedelta(hours=24, minutes=1)
+    statistics_repository.mark_noise_stat_as_updated('exxample.com', now=now)
+
+    # WHEN the email work is ran
+    workers.statistics.increment_email_sub_deltas()
+
+    # THEN no statistics will be created for exxample.com, even though there
+    # is (now) a delta report.
+    # THEN statistics will be created for exxample.com.
+    stats_data = statistics_repository.get_noise_stat('exxample.com')
+    assert stats_data.domain == 'exxample.com'
+    assert stats_data.deltas == 1
