100d5ec5517107e17e19f88a2045b5b75eb76e9e
Robert Wallhead <rwallhead@gmail.com>
2017-04-18 11:42:44 +1000

    DNS resolution via Google's Public DNS #56

---
+++ tests/test_google_dns_fallback.py
@@ -0,0 +1,37 @@
+"""Test the Google Public DNS fallback"""
+import socket
+
+from dnstwister import tools
+
+
+def _is_valid_ip(ip_string):
+    """Throws socket.error if not a valid IP address."""
+    socket.inet_aton(ip_string)
+    return True
+
+
+def test_failed_domain():
+    """Test Google Public DNS on a failing domain."""
+    domain = 'thisdomainisgoingtofaileimprettysure.com'
+    ip_addr, error = tools.google_resolve(domain)
+
+    assert not ip_addr
+    assert not error
+
+
+def test_successful_domain():
+    """Test Google Public DNS on a working domain."""
+    domain = 'dnstwister.report'
+    ip_addr, error = tools.google_resolve(domain)
+
+    assert _is_valid_ip(ip_addr)
+    assert not error
+
+
+def test_failure_to_resolve(f_httpretty):
+    """Bringing in httpretty breaks requests, this is intentional."""
+    domain = 'dnstwister.report'
+    ip_addr, error = tools.google_resolve(domain)
+
+    assert not ip_addr
+    assert error
