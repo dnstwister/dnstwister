69d89aa0b6197f2fd7291f40bf7e9fd9ccba1d2e
Robert Wallhead <rwallhead@gmail.com>
2017-04-22 11:27:59 +1000

    Added a couple of extra email generation tests

--- tests/manual/test_email_example_generation.py
+++ tests/manual/test_email_example_generation.py
@@ -1,4 +1,4 @@
-"""Write out an email to disk to easily review."""
+"""Write out email examples to disk to easily review."""
 from dnstwister.domain.email_report import EmailReport
 import dnstwister.tools.email as email_tools
 
@@ -39,5 +39,61 @@ def test_generate_example_email():
     )
 
     with open('tests/manual/example_email.html', 'wb') as email_f:
+        email_f.write(FORMATTING)
+        email_f.write(template)
+
+
+def test_generate_example_email_no_noisy():
+    """Email when no noisy domains."""
+    new = (
+        ('www.examp1e.com', '127.0.0.1'),
+    )
+    updated = (
+        ('www.exampl3.com', '127.0.0.1', '127.0.0.2'),
+    )
+    deleted = (
+        'www.examplle.com',
+        'www.eeexamplez.com',
+        'wwwexample.com',
+    )
+    noisy = set()
+
+    report = EmailReport(new, updated, deleted, noisy)
+
+    template = email_tools.render_email(
+        'report.html',
+        domain='www.example.com',
+        report=report,
+        unsubscribe_link='https://dnstwister.report/...',
+    )
+
+    with open('tests/manual/example_email_no_noisy.html', 'wb') as email_f:
+        email_f.write(FORMATTING)
+        email_f.write(template)
+
+
+
+
+def test_generate_example_all_noisy():
+    """Email when just noisy domains."""
+    new = set()
+    updated = set()
+    deleted = set()
+    noisy = (
+        'www.examplle.com',
+        'www.eeexamplez.com',
+        'wwwexample.com',
+    )
+
+    report = EmailReport(new, updated, deleted, noisy)
+
+    template = email_tools.render_email(
+        'report.html',
+        domain='www.example.com',
+        report=report,
+        unsubscribe_link='https://dnstwister.report/...',
+    )
+
+    with open('tests/manual/example_email_all_noisy.html', 'wb') as email_f:
         email_f.write(FORMATTING)
         email_f.write(template)
