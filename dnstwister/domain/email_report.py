21f6eaa132c3e0622778b5a6eea4ae89e411b488
Robert Wallhead <rwallhead@gmail.com>
2017-04-22 10:45:46 +1000

    Re-worked the worker to not send noisy domains more than once a week

--- dnstwister/domain/email_report.py
+++ dnstwister/domain/email_report.py
@@ -12,35 +12,28 @@ class EmailReport(object):
     @property
     def new(self):
         """Return the new non-noisy results we want to alert on."""
-        if self._new is not None and len(self._new) > 0:
-            try:
-                return [(domain, ip)
-                        for (domain, ip)
-                        in self._new
-                        if domain not in self._noisy]
-            except:
-                import pdb; pdb.set_trace()
+        return [(domain, ip)
+                for (domain, ip)
+                in self._new
+                if domain not in self._noisy]
 
     @property
     def updated(self):
         """Return the updated non-noisy results we want to alert on."""
-        if self._updated is not None and len(self._updated) > 0:
-            return [(domain, ip1, ip2)
-                    for (domain, ip1, ip2)
-                    in self._updated
-                    if domain not in self._noisy]
+        return [(domain, ip1, ip2)
+                for (domain, ip1, ip2)
+                in self._updated
+                if domain not in self._noisy]
 
     @property
     def deleted(self):
         """Return the deleted non-noisy results we want to alert on."""
-        if self._deleted is not None and len(self._deleted) > 0:
-            return [domain
-                    for domain
-                    in self._deleted
-                    if domain not in self._noisy]
+        return [domain
+                for domain
+                in self._deleted
+                if domain not in self._noisy]
 
     @property
     def noisy(self):
         """Return the noisy results."""
-        if self._noisy is not None and len(self._noisy) > 0:
-            return list(self._noisy)
+        return list(self._noisy)
