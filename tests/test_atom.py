48d1ec724f134c278812678c4dae4c0c9007fdbe
Robert Wallhead <rwallhead@gmail.com>
2017-11-17 11:18:29 +1100

    Rest of atom integration test with Unicode

--- tests/test_atom.py
+++ tests/test_atom.py
@@ -479,21 +479,22 @@ class TestAtomUnicode(unittest.TestCase):
         """).strip()
 
     @mock.patch('dnstwister.repository.db', patches.SimpleKVDatabase())
-    def test_deleted_items_appear_in_rss(self):
-        """Tests that deleted items in delta reports appear in the RSS.
+    def test_updated_and_deleted_items_appear_in_rss(self):
+        """Tests that updated and deleted items in delta reports appear in the
+        RSS.
         """
         repository = dnstwister.repository
 
         # We need a domain to get the feed for.
-        domain = 'www.example.com'
+        domain = u'www.\u0454xample.com'
 
         # We can calculate a delta though.
         update_date = datetime.datetime(2016, 2, 28, 11, 10, 34)
         repository.update_delta_report(
             domain, {
                 'new': [('www.examp1e.com', '127.0.0.1')],
-                'updated': [('wwwexa.mple.com', '127.0.0.1', '127.0.0.2')],
-                'deleted': ['www.eeexample.com', 'www2.example.com.au'],
+                'updated': [(u'www\u0454xa.mple.com', '127.0.0.1', '127.0.0.2')],
+                'deleted': [u'www.\u0454xampl\u0454.com', 'www2.example.com.au'],
             },
             update_date
         )
@@ -501,58 +502,58 @@ class TestAtomUnicode(unittest.TestCase):
         # Clear the webapp cache
         dnstwister.cache.clear()
 
-        res = self.app.get('/atom/{}'.format(base64.b64encode(domain))).follow()
+        res = self.app.get('/atom/{}'.format(tools.encode_domain(domain)))
         assert str(res) == textwrap.dedent("""
             Response: 200 OK
             Content-Type: application/atom+xml; charset=utf-8
             <?xml version="1.0" encoding="utf-8"?>
             <feed xmlns="http://www.w3.org/2005/Atom">
-              <title type="text">dnstwister report for www.example.com</title>
-              <id>http://localhost:80/atom/7777772e6578616d706c652e636f6d</id>
+              <title type="text">dnstwister report for www.\xd1\x94xample.com (www.xn--xample-9uf.com)</title>
+              <id>http://localhost:80/atom/7777772e786e2d2d78616d706c652d3975662e636f6d</id>
               <updated>2016-02-28T11:10:34Z</updated>
-              <link href="http://localhost:80/search/7777772e6578616d706c652e636f6d" />
-              <link href="http://localhost:80/atom/7777772e6578616d706c652e636f6d" rel="self" />
+              <link href="http://localhost:80/search/7777772e786e2d2d78616d706c652d3975662e636f6d" />
+              <link href="http://localhost:80/atom/7777772e786e2d2d78616d706c652d3975662e636f6d" rel="self" />
               <generator>Werkzeug</generator>
-              <entry xml:base="http://localhost:80/atom/7777772e6578616d706c652e636f6d">
+              <entry xml:base="http://localhost:80/atom/7777772e786e2d2d78616d706c652d3975662e636f6d">
                 <title type="text">NEW: www.examp1e.com</title>
                 <id>new:www.examp1e.com:127.0.0.1:1456657834.0</id>
                 <updated>2016-02-28T11:10:34Z</updated>
                 <published>2016-02-28T11:10:34Z</published>
-                <link href="http://localhost:80/search/7777772e6578616d706c652e636f6d" />
+                <link href="http://localhost:80/search/7777772e786e2d2d78616d706c652d3975662e636f6d" />
                 <author>
                   <name>dnstwister</name>
                 </author>
                 <content type="html">&lt;h1&gt;IP: 127.0.0.1&lt;/h1&gt;
             &lt;a href=&quot;https://dnstwister.report/analyse/7777772e6578616d7031652e636f6d&quot;&gt;analyse&lt;/a&gt;</content>
               </entry>
-              <entry xml:base="http://localhost:80/atom/7777772e6578616d706c652e636f6d">
-                <title type="text">UPDATED: wwwexa.mple.com</title>
-                <id>updated:wwwexa.mple.com:127.0.0.1:127.0.0.2:1456657834.0</id>
+              <entry xml:base="http://localhost:80/atom/7777772e786e2d2d78616d706c652d3975662e636f6d">
+                <title type="text">UPDATED: www\xd1\x94xa.mple.com (xn--wwwxa-d2e.mple.com)</title>
+                <id>updated:xn--wwwxa-d2e.mple.com:127.0.0.1:127.0.0.2:1456657834.0</id>
                 <updated>2016-02-28T11:10:34Z</updated>
                 <published>2016-02-28T11:10:34Z</published>
-                <link href="http://localhost:80/search/7777772e6578616d706c652e636f6d" />
+                <link href="http://localhost:80/search/7777772e786e2d2d78616d706c652d3975662e636f6d" />
                 <author>
                   <name>dnstwister</name>
                 </author>
                 <content type="html">&lt;h1&gt;IP: 127.0.0.1 &amp;gt; 127.0.0.2&lt;/h1&gt;
-            &lt;a href=&quot;https://dnstwister.report/analyse/7777776578612e6d706c652e636f6d&quot;&gt;analyse&lt;/a&gt;</content>
+            &lt;a href=&quot;https://dnstwister.report/analyse/786e2d2d77777778612d6432652e6d706c652e636f6d&quot;&gt;analyse&lt;/a&gt;</content>
               </entry>
-              <entry xml:base="http://localhost:80/atom/7777772e6578616d706c652e636f6d">
-                <title type="text">DELETED: www.eeexample.com</title>
-                <id>deleted:www.eeexample.com:1456657834.0</id>
+              <entry xml:base="http://localhost:80/atom/7777772e786e2d2d78616d706c652d3975662e636f6d">
+                <title type="text">DELETED: www.\xd1\x94xampl\xd1\x94.com (www.xn--xampl-91ef.com)</title>
+                <id>deleted:www.xn--xampl-91ef.com:1456657834.0</id>
                 <updated>2016-02-28T11:10:34Z</updated>
                 <published>2016-02-28T11:10:34Z</published>
-                <link href="http://localhost:80/search/7777772e6578616d706c652e636f6d" />
+                <link href="http://localhost:80/search/7777772e786e2d2d78616d706c652d3975662e636f6d" />
                 <author>
                   <name>dnstwister</name>
                 </author>
               </entry>
-              <entry xml:base="http://localhost:80/atom/7777772e6578616d706c652e636f6d">
+              <entry xml:base="http://localhost:80/atom/7777772e786e2d2d78616d706c652d3975662e636f6d">
                 <title type="text">DELETED: www2.example.com.au</title>
                 <id>deleted:www2.example.com.au:1456657834.0</id>
                 <updated>2016-02-28T11:10:34Z</updated>
                 <published>2016-02-28T11:10:34Z</published>
-                <link href="http://localhost:80/search/7777772e6578616d706c652e636f6d" />
+                <link href="http://localhost:80/search/7777772e786e2d2d78616d706c652d3975662e636f6d" />
                 <author>
                   <name>dnstwister</name>
                 </author>
@@ -565,15 +566,15 @@ class TestAtomUnicode(unittest.TestCase):
         """Tests that reading a feed is logged."""
         repository = dnstwister.repository
 
-        domain = 'www.example.com'
-        b64domain = base64.b64encode(domain)
+        domain = u'www.\u0454xample.com'
+        get_param = tools.encode_domain(domain)
 
         # Read dates are None by default
         read_date = repository.delta_report_last_read(domain)
         assert read_date is None
 
         # Registering a feed will update the read date
-        self.app.get('/atom/{}'.format(b64domain)).follow()
+        self.app.get('/atom/{}'.format(get_param))
         read_date = repository.delta_report_last_read(domain)
         assert type(read_date) is datetime.datetime
 
@@ -588,7 +589,7 @@ class TestAtomUnicode(unittest.TestCase):
 
         # Reading a feed will update the read date
         read_date = repository.delta_report_last_read(domain)
-        self.app.get('/atom/{}'.format(b64domain)).follow()
+        self.app.get('/atom/{}'.format(get_param))
         read_date2 = repository.delta_report_last_read(domain)
 
         assert read_date2 > read_date
@@ -598,13 +599,13 @@ class TestAtomUnicode(unittest.TestCase):
         """Tests that you can unregister domains."""
         repository = dnstwister.repository
 
-        domain = 'www.example.com'
-        b64domain = base64.b64encode(domain)
+        domain = u'www.\u0454xample.com'
+        get_param = tools.encode_domain(domain)
 
         assert not repository.is_domain_registered(domain)
         assert repository.db.data == {}
 
-        self.app.get('/atom/{}'.format(b64domain)).follow()
+        self.app.get('/atom/{}'.format(get_param))
         repository.update_delta_report(
             domain, {
                 'new': [('www.examp1e.com', '127.0.0.1')],
