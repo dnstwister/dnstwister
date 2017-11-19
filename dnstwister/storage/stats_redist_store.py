d86f2a39d177c81f1c2e33b0d115fcbf5902337d
Robert Wallhead <rwallhead@gmail.com>
2017-11-19 19:29:19 +1100

    Updated query to be simpler

--- dnstwister/storage/stats_redist_store.py
+++ dnstwister/storage/stats_redist_store.py
@@ -7,18 +7,18 @@ Reads from Heroku Dataclip of domains in delta reports, writes to Redis.
 
 Dataclip is:
 
-    select distinct delta_value->0 as domain
+    select distinct delta_value->>0 as domain
     from (
       select
-        json_array_elements(data.value::json->'new')::jsonb as delta_value
+        json_array_elements(data.value::json->'new') as delta_value
       from data
       union all
       select
-        json_array_elements(data.value::json->'updated')::jsonb as delta_value
+        json_array_elements(data.value::json->'updated') as delta_value
       from data
       union all
       select
-        json_array_elements(data.value::json->'deleted')::jsonb as delta_value
+        json_array_elements(data.value::json->'deleted') as delta_value
       from data
     ) as delta_values
 """
