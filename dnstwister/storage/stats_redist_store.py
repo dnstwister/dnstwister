f515af90e81ba37c80dd61953393941d9afeba40
Robert Wallhead <rwallhead@gmail.com>
2017-11-25 15:51:11 +1100

    Basic working, err, worker

--- dnstwister/storage/stats_redist_store.py
+++ dnstwister/storage/stats_redist_store.py
@@ -24,10 +24,13 @@ Dataclip is:
     where delta_value->>0 != 'null'
 
 """
-import contextlib
+import datetime
 import os
+import time
 
 import redis
+
+from dnstwister import dnstwist
 
 
 EXPIRY = 604800  # 7 days in seconds.
@@ -43,7 +46,6 @@ class RedisStatsStore(object):
     def r_conn(self):
         """I am a redis connection!!!"""
         if self._conn is None:
-            print 'new'
             self._conn = redis.from_url(self._url)
         return self._conn
 
@@ -54,23 +56,26 @@ class RedisStatsStore(object):
         chosen number of seconds. That gives us a sliding window of changes
         over the period.
         """
-        pipe = self.r_conn.pipeline()
-        pipe.incr(domain)
-        pipe.expire(domain, EXPIRY)
-        pipe.execute()
+        if dnstwist.validate_domain(domain):
+            pipe = self.r_conn.pipeline()
+            pipe.incr(domain)
+            pipe.expire(domain, EXPIRY)
+            pipe.execute()
+
+    def all_noted(self):
+        """Record that all domains have been noted."""
+        now_ts = time.mktime(datetime.datetime.now().timetuple())
+        self.r_conn.set('meta:all_noted', now_ts)
+
+    def last_time_all_noted(self):
+        """Return the last time that all values were updated or epoch zero."""
+        last_all_noted = self.r_conn.get('meta:all_noted')
+        if last_all_noted is None:
+            return datetime.datetime.min
+        return datetime.datetime(*time.localtime(float(last_all_noted))[:6])
 
     def is_noisy(self, domain, threshold=3):
         """A domain is noisy if it changes more than threshold times over the
         expiry window.
         """
         return self.r_conn.get(domain) > threshold
-
-
-if __name__ == '__main__':
-    rs = RedisStatsStore()
-    for i in range(10):
-        rs.note('www.example.com')
-        print 'done'
-
-    print rs.is_noisy('www.example.com')
-    print rs.is_noisy('www.facebook.com')
--- dnstwister/workers/stats.py
+++ dnstwister/workers/stats.py
@@ -1,39 +1,51 @@
 """Records stats about the system.
+
 """
+import datetime
 import os
 import time
 
 import requests
 
-from dnstwister.storage import stats_redis_store
+from dnstwister import dnstwist
+from dnstwister.storage import redis_stats_store
 
 
-def record_delta_stats(domain):
-    """Record stats based on this domain."""
-    stats_redis_store.note(domain)
+CADENCE = datetime.timedelta(seconds=86400)  # 24 hours
 
 
 def get_delta_domains():
     """Return a list of all the domains in all the delta reports."""
-    json = requests.get(os.getenv('DELTAS_URL')).json
-    return json['values']
+    json = requests.get(os.getenv('DELTAS_URL'), timeout=10).json()
+    return [domain
+            for (domain,)
+            in json['values']
+            if dnstwist.validate_domain(domain)]
 
 
 def main():
     """Main code for worker."""
     while True:
 
-        print 'Starting stats processing...'
         start = time.time()
+        print 'Starting stats processing...'
 
-        store = stats_redis_store.RedisStatsStore()
+        store = redis_stats_store.RedisStatsStore()
+
+        last_run = store.last_time_all_noted()
+        age = datetime.datetime.now() - last_run
+        if age < CADENCE:
+            print 'Sleeping, < 24 hours since last run.'
+            time.sleep((CADENCE - age).total_seconds())
+            continue
+
         for domain in get_delta_domains():
             store.note(domain)
+        store.all_noted()
 
         print 'All stats processed in {} seconds'.format(
             round(time.time() - start, 2)
         )
-        time.sleep(60)
 
 
 if __name__ == '__main__':
