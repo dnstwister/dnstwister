c88a4a37ca6effd705d8f523a6bd8f3cc0866421
Robert Wallhead <rwallhead@gmail.com>
2017-12-10 11:57:41 +1100

    Worked out a way to improve testing of the worker launching - this improves coverage if nothing else

---
+++ tests/integration/test_worker_launching.py
@@ -0,0 +1,55 @@
+"""Tests of executing the workers as modules."""
+import runpy
+
+import pytest
+
+
+def test_launching_stats_worker(monkeypatch):
+    """We're happy for this to fail, but we just want to prove it's module-
+    launchable.
+
+    This simulates:
+
+        python -m dnstwister.workers.stats
+
+    """
+    monkeypatch.delenv('REDIS_URL', raising=False)
+
+    with pytest.raises(Exception) as ex:
+        runpy.run_module('dnstwister.workers.stats', run_name='__main__')
+
+    assert ex.value.message == 'REDIS connection configuration not set!'
+
+
+def test_launching_deltas_worker(monkeypatch):
+    """We're happy for this to fail, but we just want to prove it's module-
+    launchable.
+
+    This simulates:
+
+        python -m dnstwister.workers.deltas
+
+    """
+    monkeypatch.delenv('DATABASE_URL', raising=False)
+
+    with pytest.raises(Exception) as ex:
+        runpy.run_module('dnstwister.workers.deltas', run_name='__main__')
+
+    assert ex.value.message == 'DATABASE_URL'
+
+
+def test_launching_emails_worker(monkeypatch):
+    """We're happy for this to fail, but we just want to prove it's module-
+    launchable.
+
+    This simulates:
+
+        python -m dnstwister.workers.emails
+
+    """
+    monkeypatch.delenv('DATABASE_URL', raising=False)
+
+    with pytest.raises(Exception) as ex:
+        runpy.run_module('dnstwister.workers.email', run_name='__main__')
+
+    assert ex.value.message == 'DATABASE_URL'
