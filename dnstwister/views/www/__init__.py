70a4493e306a1227edb46306648ab523be0bda28
Robert Wallhead <rwallhead@gmail.com>
2018-01-21 09:05:32 +1100

    Added basic security.txt
    https://securitytxt.org

--- dnstwister/views/www/__init__.py
+++ dnstwister/views/www/__init__.py
@@ -9,6 +9,7 @@ from dnstwister import cache
 @app.route(r'/favicon.ico')
 @app.route(r'/robots.txt')
 @app.route(r'/sitemap.xml')
+@app.route(r'/security.txt')
 def favicon():
     """Favicon (because some clients don't read the link tag) as well as some
     other static files.
