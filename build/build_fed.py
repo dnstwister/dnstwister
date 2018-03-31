6ee1c7a9a50781bd4bfb170e6590bf92c2835db2
Robert Wallhead <rwallhead@gmail.com>
2018-04-01 09:19:13 +1000

    Test server now does FED builds too

--- build/build_fed.py
+++ build/build_fed.py
@@ -1,11 +1,11 @@
-"""Build the static files.
-
-Requires: pip install csscompressor
-
-Cloudflare compresses JS for us.
-"""
-import csscompressor
+"""Build the static files."""
+import os
 import sys
+import time
+
+import csscompressor
+import watchdog.events
+import watchdog.observers
 
 
 maps = {
@@ -50,12 +50,13 @@ maps = {
 }
 
 
-def build():
+def build(root):
     print 'building...'
     for (dest, srcs) in maps.items():
         dest_data = []
-        for src in srcs:
-            with open('sources/{}'.format(src), 'rb') as srcf:
+        for src_file in srcs:
+            src_path = os.path.join(root, 'sources/{}'.format(src_file))
+            with open(src_path, 'rb') as srcf:
                 dest_data.append(srcf.read())
 
         dest_data = '\n'.join(dest_data)
@@ -63,29 +64,29 @@ def build():
         if dest.endswith('.css'):
             dest_data = csscompressor.compress(dest_data)
 
-        with open(dest, 'wb') as destf:
+        with open(os.path.join(root, dest), 'wb') as destf:
             destf.write(dest_data)
 
 
+def monitor(root):
+
+    class Handler(watchdog.events.FileSystemEventHandler):
+        def on_any_event(*args):
+            build(root)
+
+    observer = watchdog.observers.Observer()
+    observer.schedule(Handler(), os.path.join(root, 'sources/'))
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
 if __name__ == '__main__':
-
-    build()
+    build('../')
     if sys.argv[-1] == '--watch':
-        import time
-        import watchdog.events
-        import watchdog.observers
-
-        class Handler(watchdog.events.FileSystemEventHandler):
-            def on_any_event(*args):
-                build()
-
-        observer = watchdog.observers.Observer()
-        observer.schedule(Handler(), './sources')
-        observer.start()
-
-        try:
-            while True:
-                time.sleep(0.1)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
+        monitor('../')
--- test_server.py
+++ test_server.py
@@ -1,15 +1,22 @@
 """Launch."""
 import os
 
+import threading
 import waitress
 import werkzeug.serving
 
+import build.build_fed as build_fed
 import dnstwister
 
 
 @werkzeug.serving.run_with_reloader
 def serve():
     """Run waitress, but reload with file system changes."""
+
+    def builder_thread():
+        build_fed.build('dnstwister/static/')
+        build_fed.monitor('dnstwister/static/')
+    threading.Thread(target=builder_thread).start()
 
     # Allow for template changes without manual restart.
     # At least until https://github.com/pallets/flask/pull/1910 is merged...
@@ -20,7 +27,3 @@ def serve():
         host='0.0.0.0',
         port=5000,
     )
-
-
-if __name__ == '__main__':
-    serve()
