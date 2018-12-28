b353c986fdf77cbc9c6fe215322d1aab6bbee92a
Robert Wallhead <rwallhead@gmail.com>
2018-12-28 11:57:23 +1100

    Cut over to standardjs, starting on some modules

--- dnstwister/static/sources/search.js
+++ dnstwister/static/sources/search.js
@@ -1,163 +1,171 @@
-var updatedProgress = function(checkedCount, resolvedCount) {
+/* global jsonpipe, Velocity, XMLHttpRequest */
+var ui = (function () {
+  var reportShown = false
 
-    var checkedCountElem = document.getElementById('checked_count');
-    var resolvedCountElem = document.getElementById('resolved_count');
+  var updatedProgress = function (checkedCount, resolvedCount) {
+    var checkedCountElem = document.getElementById('checked_count')
+    var resolvedCountElem = document.getElementById('resolved_count')
+    var reportTableElem = document.getElementById('main_report')
 
     if (checkedCount > 0) {
-        checkedCountElem.innerHTML = checkedCount;
+      checkedCountElem.innerHTML = checkedCount
     }
 
     if (resolvedCount > 0) {
-        resolvedCountElem.innerHTML = resolvedCount;
+      resolvedCountElem.innerHTML = resolvedCount
+      if (reportShown === false) {
+        reportShown = true
+        reportTableElem.style.display = 'table'
+      }
     }
-}
+  }
 
-var startProgressDots = function () {
+  var startProgressDots = function () {
+    var searchDotsElem = document.getElementById('search_dots')
 
-    var searchDotsElem = document.getElementById('search_dots');
+    return setInterval(function () {
+      var dotsCount = (searchDotsElem.firstChild || []).length
+      var nextDotsCount = (dotsCount + 1) % 4
 
-    return setInterval(function() {
-        var dotsCount  = (searchDotsElem.firstChild || []).length;
-        var nextDotsCount = (dotsCount + 1) % 4;
+      var newDots = ''
+      for (var i = 0; i < nextDotsCount; i++) {
+        newDots += '.'
+      }
+      searchDotsElem.innerHTML = newDots
+    }, 350)
+  }
 
-        var newDots = '';
-        for(var i = 0; i < nextDotsCount; i++) {
-            newDots += '.';
+  var markProgressAsDone = function () {
+    var progressElem = document.getElementsByClassName('search_progress')[0]
+    progressElem.innerHTML = 'Done!'
+
+    Velocity(document.getElementsByClassName('wip_text'), 'fadeOut', { duration: 500, delay: 250 })
+    Velocity(progressElem, 'slideUp', { duration: 500, delay: 1500 })
+    Velocity(document.getElementsByClassName('search_result'), { 'font-size': '150%' }, { duration: 500, delay: 1500 })
+  }
+
+  return {
+    updatedProgress: updatedProgress,
+    startProgressDots: startProgressDots,
+    markProgressAsDone: markProgressAsDone
+  }
+})()
+
+var resolve = function (encodedDomain, callback) {
+  var request = new XMLHttpRequest()
+  var url = '/api/ip/' + encodedDomain
+  request.open('GET', url)
+  request.send()
+  request.onreadystatechange = (e) => {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        var responseText = request.responseText
+        var response = JSON.parse(responseText)
+        if (response.error === false) {
+          callback(response.ip)
+        } else {
+          callback(null)
         }
-        searchDotsElem.innerHTML = newDots;
-    }, 350);
-}
-
-var markProgressAsDone = function () {
-    var progressElem = document.getElementsByClassName('search_progress')[0];
-    progressElem.innerHTML = 'Done!';
-
-    Velocity(document.getElementsByClassName('wip_text'), 'fadeOut', { duration: 500, delay: 250 });
-    Velocity(progressElem, 'slideUp', { duration: 500, delay: 1500 });
-    Velocity(document.getElementsByClassName('search_result'), {'font-size': '150%'}, { duration: 500, delay: 1500 });
-}
-
-var resolve = function(encoded_domain, callback) {
-    const request = new XMLHttpRequest();
-    const url='/api/ip/' + encoded_domain;
-    request.open("GET", url);
-    request.send();
-    request.onreadystatechange = (e) => {
-        if(request.readyState === 4) {
-            if(request.status === 200) {
-                var responseText = request.responseText;
-                var response = JSON.parse(responseText);
-                if (response.error === false) {
-                    callback(response.ip);
-                }
-                else {
-                    callback(null);
-                }
-            }
-            else {
-                callback(null);
-            }
-        }
+      } else {
+        callback(null)
+      }
     }
+  }
 }
 
-var reportRowElem = function(domain, ipText, show) {
-    var rowElem = document.createElement('tr');
-    var domainCellElem = document.createElement('td');
-    var ipCellElem = document.createElement('td');
+var reportRowElem = function (domain, ipText, show) {
+  var rowElem = document.createElement('tr')
+  var domainCellElem = document.createElement('td')
+  var ipCellElem = document.createElement('td')
 
-    domainCellElem.appendChild(document.createTextNode(domain));
-    ipCellElem.appendChild(document.createTextNode(ipText));
+  domainCellElem.appendChild(document.createTextNode(domain))
+  ipCellElem.appendChild(document.createTextNode(ipText))
 
-    rowElem.appendChild(domainCellElem);
-    rowElem.appendChild(ipCellElem);
+  rowElem.appendChild(domainCellElem)
+  rowElem.appendChild(ipCellElem)
 
-    rowElem.className = 'domain-row';
-    if (show === true) {
-        rowElem.className = 'resolved';
-    }
+  rowElem.className = 'domain-row'
+  if (show === true) {
+    rowElem.className = 'resolved'
+  }
 
-    return rowElem;
+  return rowElem
 }
 
+var search = function (encodedDomain) {
+  var checkedCount = 0
+  var resolvedCount = 0
+  var resolveQueue = []
+  var startedResolving = false
+  var allFound = false
+  var cleaningUp = false
 
-var search = function(encoded_domain) {
+  var reportElem = document.getElementById('report_target')
 
-    var checkedCount = 0;
-    var resolvedCount = 0;
-    var resolveQueue = [];
-    var startedResolving = false;
-    var allFound = false;
-    var cleaningUp = false;
+  var progressTimer = ui.startProgressDots()
 
-    var reportElem = document.getElementById('report_target');
-
-    var progressTimer = startProgressDots();
-
-    var resolveNext = function(queue) {
-        var data = queue.pop();
-        if (data === undefined) {
-            if (allFound === true) {
-                if (cleaningUp === false) {
-                    cleaningUp = true;
-                    clearInterval(progressTimer);
-                    markProgressAsDone();
-                }
-                return;
-            }
-            else {
-                // If queue exhausted, wait for more.
-                setTimeout(function() {
-                    resolveNext(queue);
-                }, 1000);
-                return;
-            }
+  var resolveNext = function (queue) {
+    var data = queue.pop()
+    if (data === undefined) {
+      if (allFound === true) {
+        if (cleaningUp === false) {
+          cleaningUp = true
+          clearInterval(progressTimer)
+          ui.markProgressAsDone()
         }
-
-        resolve(data.ed, function(ip) {
-            checkedCount += 1;
-            updatedProgress(checkedCount, resolvedCount);
-
-            if (ip === null) {
-                reportElem.appendChild(
-                    reportRowElem(data.d, data.fuzzer, 'Error!', true)
-                );
-                resolveNext(queue);
-                return;
-            }
-            else if (ip === false) {
-                reportElem.appendChild(
-                    reportRowElem(data.d, data.fuzzer, 'None resolved', false)
-                );
-                resolveNext(queue);
-                return;
-            }
-
-            resolvedCount += 1;
-            updatedProgress(checkedCount, resolvedCount);
-            reportElem.appendChild(
-                reportRowElem(data.d, ip, true)
-            );
-
-            resolveNext(queue);
-        });
+        return
+      } else {
+        // If queue exhausted, wait for more.
+        setTimeout(function () {
+          resolveNext(queue)
+        }, 1000)
+        return
+      }
     }
 
-    jsonpipe.flow('/api/fuzz_chunked/' + encoded_domain, {
-        'success': function(data) {
-            resolveQueue.push(data);
+    resolve(data.ed, function (ip) {
+      checkedCount += 1
+      ui.updatedProgress(checkedCount, resolvedCount)
 
-            if (startedResolving !== true) {
-                startedResolving = true;
-                for(var i = 0; i < 5; i++) {
-                    setTimeout(function() {
-                        resolveNext(resolveQueue);
-                    }, 500);
-                }
-            }
-        },
-        'complete': function() {
-            allFound = true;
+      if (ip === null) {
+        reportElem.appendChild(
+          reportRowElem(data.d, data.fuzzer, 'Error!', true)
+        )
+        resolveNext(queue)
+        return
+      } else if (ip === false) {
+        reportElem.appendChild(
+          reportRowElem(data.d, data.fuzzer, 'None resolved', false)
+        )
+        resolveNext(queue)
+        return
+      }
+
+      resolvedCount += 1
+      ui.updatedProgress(checkedCount, resolvedCount)
+      reportElem.appendChild(
+        reportRowElem(data.d, ip, true)
+      )
+
+      resolveNext(queue)
+    })
+  }
+
+  jsonpipe.flow('/api/fuzz_chunked/' + encodedDomain, {
+    'success': function (data) {
+      resolveQueue.push(data)
+
+      if (startedResolving !== true) {
+        startedResolving = true
+        for (var i = 0; i < 5; i++) {
+          setTimeout(function () {
+            resolveNext(resolveQueue)
+          }, 500)
         }
-    });
+      }
+    },
+    'complete': function () {
+      allFound = true
+    }
+  })
 }
