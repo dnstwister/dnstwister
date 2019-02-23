4cc8da3a55082f0901428d1981c64a56b0526b74
Robert Wallhead <rwallhead@gmail.com>
2019-02-23 16:16:05 +1100

    Some progress :)

--- tests/test_js_vs_python.py
+++ tests/test_js_vs_python.py
@@ -1,4 +1,6 @@
 """A test that compares the two implementations."""
+# -*- coding: UTF-8 -*-
+import io
 import os
 
 import js2py
@@ -7,9 +9,16 @@ import js2py
 def test_js_module_matches_python_module():
     """Run the JS in the browser, compare to the python output."""
     tld_js = load_js('tld.min.js')
-    dnstwist_js = load_js('dnstwist.js')
+    dnstwist_js = load_js('dnstwist.js').strip()[:-2]  # Parenthesis break it?
 
-    js2py.eval_js(tld_js + dnstwist_js)
+#    js2py.eval_js(tld_js)
+    ev = js2py.eval_js(dnstwist_js)
+
+    import pdb; pdb.set_trace()
+
+
+#    from js2py.internals import seval
+#    seval.eval_js_vm(tld_js + '\n' + dnstwist_js)
 
 
 def load_js(filename):
@@ -21,5 +30,8 @@ def load_js(filename):
         filename
     )
 
-    with open(js_src_path, 'r') as js_f:
-        return js_f.read()
+    return io.open(
+        js_src_path,
+        mode='r',
+        encoding='utf-8'
+    ).read()
